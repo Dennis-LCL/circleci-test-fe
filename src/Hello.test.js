9ae93d0ea8ffa6225bc4da3bdeafe69718ce415e
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-07-14 13:34:00 +0800

    Build Hello component and add circleci folder and config.yml.

---
+++ src/Hello.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import "@testing-library/jest-dom/extend-expect";
+import Hello from "./Hello";
+
+describe("Fron-End Unit Test.", () => {
+  it("should show 'Hello Marco!' on the page.", () => {
+    const { getByText } = render(<Hello />);
+    expect(getByText("Hello Marco!")).toBeInTheDocument();
+  });
+});
